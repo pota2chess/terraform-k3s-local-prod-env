b9a09a3c4eb9101d936d1218ce7f557ee8897659
pota2chess <311020139+pota2chess@users.noreply.github.com>
2026-09-03 09:58:12 +0300

    Update server.js

--- apps/birthday/src/server.js
+++ apps/birthday/src/server.js
@@ -4,7 +4,7 @@ var handleRequest = function(request, response) {
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
   var b = Math.floor(Math.random() * 256);
-  var coloredText = '\x1b[38;2;' + r + ';' + g + ';' + b + 'mС днем рождения, Илья!\x1b[0m\n';
+  var coloredText = '\x1b[38;2;' + r + ';' + g + ';' + b + 'mHappy birthday!\x1b[0m\n';
   
   response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
   response.end(coloredText);
